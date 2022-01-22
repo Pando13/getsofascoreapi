53613051cf23efb4a9453fe52733bcdc54d49a2c
Pando13 <69055445+Pando13@users.noreply.github.com>
2022-01-22 16:38:26 +0100

    inizio print informazioni

--- tutto.py
+++ tutto.py
@@ -1,10 +1,23 @@
 import json
+from datetime import datetime
 
 jsonfile = open('jsonpingpong.json', 'r')
 jsondata=jsonfile.read()
 
-obj = json.loads(jsondata)
+events = json.loads(jsondata)
 
-bho = str(obj['events'])
-
-print(bho)
+for event in events['events']:
+    data = datetime.fromtimestamp(event['startTimestamp']).strftime('%H:%M - %d/%m/%Y')
+    stato = event['status']['type']
+    casa = event['homeTeam']['name']
+    ospite = event['awayTeam']['name']
+    if stato == 'notstarted':
+        print(f"{casa} vs {ospite}: Inizio alle {data}")
+    elif stato == 'inprogress':
+        print(f"{casa} vs {ospite}: in corso")
+    elif stato == 'canceled':
+        print(f"{casa} vs {ospite}: cancellato")
+    elif stato == 'finished':
+        punti_casa = event['homeScore']['normaltime']
+        punti_ospite = event['awayScore']['normaltime']
+        print(f"{casa} vs {ospite}: {punti_casa} - {punti_ospite}")
