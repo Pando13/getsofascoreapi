845eca18ca1d2b1b301ee6fc19f76753b50cd613
Pando13 <69055445+Pando13@users.noreply.github.com>
2022-01-22 13:59:44 +0100

    Update json_printer.py

--- json_printer.py
+++ json_printer.py
@@ -3,7 +3,9 @@ import requests, json
 url = "https://api.sofascore.com/api/v1/unique-tournament/15006/events/last/0"
 
 payload={}
-headers = {}
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36',
+}
 
 response = requests.request("GET", url, headers=headers, data=payload)
 
